22572ad8343f13f11aab2a36802f901ec9b1d75b
mffree <matthewfreeman63@gmail.com>
2025-02-13 06:37:22 +0000

    final

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,4 +2,21 @@ class DiceController < ApplicationController
   def two_six # the action in route.rb
     render({ :template => "dice_templates/2d6" }) # must create a views file with this name
   end
+
+  def two_ten
+    render({ :template => "dice_templates/2d10" })
+  end
+
+  def homepage
+    render({ :template => "dice_templates/home" })
+  end
+
+  def one_twenty
+    render({ :template => "dice_templates/1d20"})
+  end
+
+  def five_four
+    render({ :template => "dice_templates/5d4"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   # get("/", { :controller => "universal", :action => "homepage"})
   get("/dice/2/6", { :controller => "dice", :action => "two_six" })
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
+  get("/", { :controller => "dice", :action => "homepage" })
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty" })
+  get("/dice/5/4", { :controller => "dice", :action => "five_four" })
   
-
 end
